acb5c0d71986eb771946778a7fd0bd75edc1a02f
dzgx <90265318+dzgx@users.noreply.github.com>
2025-07-11 09:25:13 +0800

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,21 @@
+Trigger_Length = 512
+
+Trigger_Initial_Value = 0.01  # 0.005 best
+
+X_Epsilon = 5               # 0.02 best
+
+T_Epsilon = 1.0              # 0.02 best
+
+Random_Samples = 1000
+
+EPOCH = 1000
+
+BATCH_SIZE = 16
+
+Snr_DB = 30
+
+Target_Label = 0
+
+rate = 0.01
+
+scale = 1.0
